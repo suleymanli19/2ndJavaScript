3fb4ea1b620383f6b0941d336e49fd75bdbafa6e
Ismayil Suleymanli <sleymanl02@bk.ru>
2021-11-27 19:26:34 +0400

    Commited

---
+++ script.js
@@ -0,0 +1,17 @@
+let a = 23;
+console.log(a);
+
+if (a > 0 && a < 6) {
+    console.log("Korpe")
+} else if (a > 6 && a < 12) {
+    console.log("Uwaq")
+} else if (a > 12 && a < 18) {
+    console.log("Yeniyetme")
+} else if (a > 18 && a < 30) {
+    console.log("Genc")
+} else if (a > 30 && a < 50) {
+    console.log("Yawli")
+} else if (a > 50) {
+    console.log("Qoca")
+}
+
